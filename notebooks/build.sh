ed54aed41dd2526ae85280731522546123646e40
Vikas Dhiman <wecacuee@users.noreply.github.com>
2022-01-24 12:31:57 -0500

    upload hw2

---
+++ notebooks/build.sh
@@ -0,0 +1,2 @@
+clang -I /home/vdhiman/.local/miniconda3/envs/xeus/include/python3.9/ \
+      -I /home/vdhiman/.local/miniconda3/envs/xeus/lib/python3.9/site-packages/numpy/
---
+++ notebooks/test.cpp
@@ -0,0 +1,6 @@
+nclude "matplotlibcpp.h"
+namespace plt = matplotlibcpp;
+int main() {
+      plt::plot({1,3,2,4});
+      plt::show();
+}
