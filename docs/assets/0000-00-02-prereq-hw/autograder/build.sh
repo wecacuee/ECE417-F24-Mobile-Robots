99613398c0e87f848d6ce8f6e2331dc4287ff243
Vikas Dhiman <wecacuee@users.noreply.github.com>
2024-09-03 13:16:32 -0400

    add templates remove autograder

--- docs/assets/0000-00-02-prereq-hw/autograder/build.sh
+++
@@ -1,3 +0,0 @@
-python3 source/generate_test_data.py
-./run_autograder
-cd source && bash zip.sh
--- docs/assets/0000-00-02-prereq-hw/autograder/source/zip.sh
+++
@@ -1,6 +0,0 @@
-zip -r autograder.zip \
-    setup.sh \
-        requirements.txt \
-    run_autograder \
-        test_data/ \
-        test_generator.py
