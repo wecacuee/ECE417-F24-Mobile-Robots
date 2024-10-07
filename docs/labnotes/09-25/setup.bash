20c870f88a523395b71909e2050e1a6f059a9131
Vikas Dhiman <wecacuee@users.noreply.github.com>
2024-10-07 15:12:42 -0400

    midterm1 notebook

--- docs/labnotes/09-25/setup.bash
+++ docs/labnotes/09-25/setup.bash
@@ -1,5 +1,5 @@
-source /opt/ros/humble/setup.bash || source /opt/ros/humble/install/setup.bash
+source /opt/ros/humble/setup.bash
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 export CYCLONEDDS_URI="file://$(pwd)/cyclonedds.xml"
 alias rosenv='printenv | grep -E "ROS|RMW_IMPLEMENTATION|AMENT|CYCLONEDDS_URI"'
-alias syncpubsub='rsync -rcavz $(pwd)/ws/src/jetbot_planning/ jetbot@141.114.195.160:~/ece498/ws/src/jetbot_planning/'
+alias syncpubsub="rsync -rcavz $(pwd)/jetbot-ece417/ jetbot@10.0.0.2:~/ece417/"
